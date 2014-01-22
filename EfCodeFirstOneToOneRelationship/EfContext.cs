6ad9b7a74f61f1d0d6983457eeea01fde7ad20ce
Md.Hasanuzzaman <shuvo009@yahoo.com>
2014-01-22 23:48:57 +0600

    first Up

---
+++ EfCodeFirstOneToOneRelationship/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirstOneToOneRelationship
+{
+    public class EfContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Product>()
+            //    .HasKey(temp => temp.StockId);
+            modelBuilder.Entity<Stock>()
+                .HasRequired(temp => temp.Product)
+                .WithRequiredPrincipal(x => x.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+  
+}
---
+++ EfCodeFirstOneToOneRelationship/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirstOneToOneRelationship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product
+            {
+                Name = "Name",
+                Stock = new Stock()
+            };
+
+            var context = new EfContext();
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            var stock = context.Stocks.Include("Product").FirstOrDefault();
+
+            Console.WriteLine(stock.Product.Name);
+
+            var testProduct = context.Products.Include("Stock").FirstOrDefault();
+            Console.WriteLine(testProduct.Stock.Quantity);
+
+            Console.Read();
+        }
+    }
+}
